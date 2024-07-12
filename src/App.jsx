b2d1cb5fadc5a3f1fcc2f64bbde1c644f06b9311
Nosihle Mthembu <bongekam16@gmail.com>
2024-07-12 11:02:31 +0200

    feat:added vite@App

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const traineeDataURL = "http://localhost:4000/trainee";
+  const [list,setList] = useState([]);
+  useEffect(() =>{
+    console.log("Succeed")
+    const fetchtraineeData = async () =>{
+      try{
+        const response = await fetch(traineeDataURL);
+        const traineeData = await response.json();
+        setList(traineeData);
+        console.log("trainee data", traineeData);
+      }
+      catch(error){
+        alert(error);
+      }
+    };
+    (async() => await fetchtraineeData())()
+  },[]
+);
+  const car = {type:"Fiat", model:"500", color:"white"};
+  const trainee = {Name:"Nosihle",
+    surname:"Mthembu",
+    location: "Imbali",
+    colour:"Black",
+    favouritecolour:"Gray",
+    age:23,
+  }
+  return (
+    <>
+      <h1>{car.model}</h1>
+      <h1>My name is {trainee.Name}</h1>
+      <p>I live at {trainee.location} and my favourite colour is {trainee.favouritecolour}.</p>
+    </>
+  )
+}
+
+export default App
